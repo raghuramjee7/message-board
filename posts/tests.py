ed9b514e13bcd9a12952c8a177bf7882723e9897
raghuramjee7 <raghuramjee@outlook.com>
2023-06-03 15:36:19 +0530

    added tests

--- posts/tests.py
+++ posts/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Post
+from django.urls import reverse
 
 # Create your tests here.
+class PostTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.post = Post.objects.create(text="This is test data")
+
+    def test_model_content(self):
+        self.assertEqual(self.post.text, "This is test data")
+
+    def test_url_exists_at_correct_location(self):
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
+    
+    def test_url_available_by_name(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+    
+    def test_template_name_correct(self):
+        response = self.client.get(reverse("home"))
+        self.assertTemplateUsed(response, "home.html")
+
+    def test_template_text(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "This is test data")
